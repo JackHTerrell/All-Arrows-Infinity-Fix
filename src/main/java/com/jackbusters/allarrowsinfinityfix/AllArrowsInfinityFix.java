1119ed5995dc1df907129ad1bf49e220e06cbf83
Jackbusters <77648717+Jackbusters@users.noreply.github.com>
2025-06-29 22:54:03 -0500

    Datapack merged into Repo

--- src/main/java/com/jackbusters/allarrowsinfinityfix/AllArrowsInfinityFix.java
+++
@@ -1,9 +0,0 @@
-package com.jackbusters.allarrowsinfinityfix;
-
-import net.minecraftforge.fml.common.Mod;
-
-@Mod(AllArrowsInfinityFix.MOD_ID)
-public class AllArrowsInfinityFix {
-    public static final String MOD_ID = "allarrowsinfinityfix";
-}
-
